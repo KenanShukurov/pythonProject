c84a012ca14442b6c10994690db3c341dc79c4df
Шукюров Кянан Агшин <Shuky_gv81@itstep.edu.az>
2023-06-04 11:50:57 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+# from random import randint
+# count = randint(1, 100)
+# print(count)
+#
+# while True:
+#     num1 = int(input("Enter your number: "))
+#
+#     if num1 >= 1 and num1 <= 100:
+#         print()
+#
+#     if num1 == 0:
+#         print("Good bye")
+#         break
+#
+#         counter = 0
+#         while True:
+#             num1 = int(input("Enter your number: "))
+#             counter += num1
+#
+#             if num1 == 0:
+#                 print(counter)
+#                 break
+#
+#
+#
+#
